9d1cf1a0056845c2731dcffb16362da6934c376a
srv-scwt <dev3.scwt@gmail.com>
2026-05-19 17:14:54 +0530

    dis priv

--- src/app/(public)/disclaimer/page.jsx
+++ src/app/(public)/disclaimer/page.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import HeroSection from "@/components/common/heroSection";
 import HTMLRender from "@/components/ui/HTMLRender";
 import styles from "@/app/common.module.css";
+import { getCommonProductsCategory } from "@/services/commonP/commonProductsCat";
+import { getCommonBanner } from "@/services/commonBanner/commonBanner.api";
+import { createImageSourceURL } from "@/utils";
 
-const page = () => {
-  const heroData = {
-    banner: "/images/blog/blogBanner.jpg",
-    title: "Disclaimer",
-  };
+const page = async() => {
+      const homeBanner = await getCommonBanner("DisclaimerHeroSection");
+      const data1 = await getCommonProductsCategory("SettingsDisclaimerContent");
+  
+    console.log(data1?.data , "d");
+    
+      const heroData = {
+        banner: createImageSourceURL(homeBanner?.data?.image) ?? "/images/board/policies_banner_large.jpg",
+        title: homeBanner?.data?.title ?? "Disclaimer",
+      };
 
- const data = `
- <h2></h2>
- <p>Electrosteel Castings Ltd. has taken all reasonable care in developing the website, and we believe that all information is accurate at the time of publication or last modification.</p>
- </br>
- <p>The Company reserves the right to make changes to this site without notice and will not be liable for any damages arising from the use of this site. Nor is it responsible for the content of any other websites linked from here.</p>
- `
   return (
     <>
       <HeroSection data={heroData} />
       <section>
         <div className={`${styles.containerLg}`}>
           <div className={`${styles.sectionContent} ${styles.customUlListing}`}>
-            <HTMLRender htmlString={data} />
+            <HTMLRender htmlString={data1?.data?.[0]?.description} />
           </div>
         </div>
       </section>
--- src/app/(public)/privacy-policy/page.jsx
+++ src/app/(public)/privacy-policy/page.jsx
@@ -2,30 +2,30 @@ import React from "react";
 import HeroSection from "@/components/common/heroSection";
 import HTMLRender from "@/components/ui/HTMLRender";
 import styles from "@/app/common.module.css";
+import { getCommonProductsCategory } from "@/services/commonP/commonProductsCat";
+import { getCommonBanner } from "@/services/commonBanner/commonBanner.api";
+import { createImageSourceURL } from "@/utils";
 
-const page = () => {
+const page = async () => {
+  const homeBanner = await getCommonBanner("PrivacyPolicyHeroSection");
+  const data1 = await getCommonProductsCategory("SettingsPrivacyPolicyContent");
+
+  console.log(data1?.data, "d");
+
   const heroData = {
-    banner: "/images/blog/blogBanner.jpg",
-    title: "Privacy Policy",
+    banner:
+      createImageSourceURL(homeBanner?.data?.image) ??
+      "/images/board/policies_banner_large.jpg",
+    title: homeBanner?.data?.title ?? "Privacy",
   };
 
- const data = `
- <h2></h2>
- <p>Thank you for visiting the Electrosteel website. Visitors are guaranteed privacy and any information collected is kept private and never shared with other organizations. It is used only by the Company to administer your request, if any.</p>
- </br>
- <p>All rights are reserved by Electrosteel Castings Ltd. The content, code and applications contained on this website, under the domain www.electrosteel.com are copyright protected. Site visitors may not reproduce, copy, or redistribute content or code in any form without written permission from Electrosteel.</p>
- <p>
- </br>
- This site has security measures in place to protect the loss, misuse and alteration of the information under our control. Unauthorized attempts to upload or change information (or otherwise cause damage to this website) are strictly prohibited.
- </p>
- `
   return (
     <>
       <HeroSection data={heroData} />
       <section>
         <div className={`${styles.containerLg}`}>
           <div className={`${styles.sectionContent} ${styles.customUlListing}`}>
-            <HTMLRender htmlString={data} />
+            <HTMLRender htmlString={data1?.data?.[0]?.description} />
           </div>
         </div>
       </section>
