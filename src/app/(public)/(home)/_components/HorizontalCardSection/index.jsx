99b93ab6d878e3df1cb5881f02eb3356a4a793ce
Sourav Kumar <155536154+srv-scwt@users.noreply.github.com>
2026-05-20 20:18:49 +0530

    slug updated

--- src/app/(public)/(home)/_components/HorizontalCardSection/index.jsx
+++ src/app/(public)/(home)/_components/HorizontalCardSection/index.jsx
@@ -76,7 +76,7 @@ export default function HorizontalCardSection({ cardData = [] }) {
                       }
                     </h3>`} />
                     <div className={styles.cardLink}>
-                      <OutlineButtonLink goto={post?.link ?? '#'} title={"Read More"} />
+                      <OutlineButtonLink goto={post?.slug ?? '#'} title={"Read More"} />
                     </div>
                   </div>
                   </div>
