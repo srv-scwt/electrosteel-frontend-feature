c9c7287ebec3c8aa639346b2d03fae17f1eb2070
subhadip121 <dev6.scwt@gmail.com>
2026-07-29 15:56:11 +0530

    update code

--- src/app/(public)/products/_components/currentManufacture/index.jsx
+++ src/app/(public)/products/_components/currentManufacture/index.jsx
@@ -12,12 +12,12 @@ const CurrentManufacture = ({ data, paints = [] }) => {
         <div className={`${styles.containerLg} !pt-0`}>
           <div className="block w-full mb-6 lg:mb-8 relative flow-root">
             <Image 
-              src="/images/paints/s-b-i-2.png" 
+              src="/images/paints/s-b-i-3.png" 
               alt="Manufacturing Excellence" 
               width={800}
               height={800}
               style={{
-                shapeOutside: `url('/images/paints/s-b-i-2.png')`,
+                shapeOutside: `url('/images/paints/s-b-i-3.png')`,
                 shapeMargin: '5rem',
               }}
               className="float-none lg:float-right w-full lg:w-[49%] xl:w-[49%] h-auto mb-4 lg:mb-0 lg:ml-8 object-contain object-right-top" 
