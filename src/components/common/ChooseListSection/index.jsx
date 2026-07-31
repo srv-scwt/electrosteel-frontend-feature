2ac07ce09b1c870a8f4aa2ffa1bd4850b6f923f2
debteamsky <deb.teamsky@gmail.com>
2026-07-31 19:21:07 +0530

    bg img fix prod

--- src/components/common/ChooseListSection/index.jsx
+++ src/components/common/ChooseListSection/index.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import styles from "./style.module.css";
 import cstyle from "@/app/common.module.css";
 import HTMLRender from "@/components/ui/HTMLRender";
+import { createImageSourceURL } from "@/utils";
 
 const ChooseListSection = ({ data }) => {
+  const backgroundImage = createImageSourceURL(data?.image, "/images/product-details/background-img.jpg");
+
   return (
     <div
       id="appications"
-      className={`${styles.backgroundImg} bg-[url('/images/product-details/background-img.jpg')]`}
+      className={styles.backgroundImg}
+      style={{ backgroundImage: `url('${backgroundImage}')` }}
     >
       <div className={styles.container}>
         <div className={`${cstyle.sectionContent} ${styles.title}`}>
