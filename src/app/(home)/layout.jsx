8210165cd0f9e6d17c5e7677b7aca920a7f757d2
Simpreative Consultancy <sandip@scwebtech.com>
2026-09-15 17:25:35 +0530

    added 2 schema in the homepage

--- src/app/(home)/layout.jsx
+++ src/app/(home)/layout.jsx
@@ -12,6 +12,23 @@ const webPageJsonLd = {
   inLanguage: "en-IN",
 };
 
+const organizationJsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  name: "Electrosteel Castings Limited",
+  alternateName: "ECL",
+  url: "https://www.electrosteel.com/",
+  logo: "https://www.electrosteel.com/images/logo.png",
+  sameAs: [
+    "https://www.facebook.com/ElectrosteelGroup/",
+    "https://x.com/ElectrosteelG",
+    "https://www.instagram.com/electrosteel_group",
+    "https://www.youtube.com/@electrosteelgroupofficial9185",
+    "https://www.linkedin.com/company/18119245/",
+    "https://en.wikipedia.org/wiki/Electrosteel_Castings",
+  ],
+};
+
 // The schema is rendered here rather than in page.jsx because loading.js
 // wraps the page in a streamed Suspense boundary but not this layout. Inside
 // that boundary React ships the tag in a hidden container and moves it into
@@ -20,6 +37,7 @@ export default function HomeLayout({ children }) {
   return (
     <>
       <JsonLd data={webPageJsonLd} />
+      <JsonLd data={organizationJsonLd} />
       {children}
     </>
   );
